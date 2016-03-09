61a4821e8b68c33b1ec6606b108023fbcf13cb47
Ruebel Holmes <treyholmes@yahoo.com>
2016-03-09 15:50:33 -0600

    Tests and one solution

---
+++ TestWk2D3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWk2D3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bob bob = new Bob();
+            string remark = "";
+        }
+    }
+
+    public class Bob
+    {
+        public string Hey(string remark)
+        {
+            if (remark.EndsWith("!"))
+            {
+                return "Whoa, chill out!";
+            }
+
+            return "Whatever.";
+        }
+
+
+
+
+
+
+
+    }
+}
