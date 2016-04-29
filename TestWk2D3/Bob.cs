72ccb33d87552d5ba8462a9c777c11b6fb86cd8e
Ruebel Holmes <treyholmes@yahoo.com>
2016-04-29 14:53:11 -0500

    All tests pass

--- TestWk2D3/Bob.cs
+++ TestWk2D3/Bob.cs
@@ -1,56 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace TestWk2D3
 {
-    public partial class Bob
+    public class Bob
     {
         public string hey(string remark)
         {
+            if (Yelling(remark))
+                return "Whoa, chill out!";
+
+
             if (Silence(remark))
                 return "Fine. Be that way!";
 
             if (Question(remark))
                 return "Sure.";
 
-            if (Yelling(remark))
-                return "Whoa, chill out!";
 
             return "Whatever.";
         }
 
-        public bool Silence(string remark)
+        private bool Silence(string remark) => string.IsNullOrWhiteSpace(remark);
+
+        private bool Question(string remark) => remark.EndsWith("?");
+
+        private bool Yelling(string remark)
         {
-            return remark == null || remark.Trim() == " ";
+            var lettersOnly = remark.Where(char.IsLetter).ToList();
+            return lettersOnly.Any() && lettersOnly.All(char.IsUpper);
         }
-
-        public bool Question(string remark) => remark.EndsWith("?");
-
-        public bool Yelling(string remark)
-        {
-            if (remark.Any(t => char.IsDigit(t) && remark.EndsWith("!")))
-            { 
-                return true;
-            }
-            return remark == ReplaceNumbers(remark.ToUpper());
-        }
-
-        private string ReplaceNumbers(string remark)
-        {
-            for (int i = 0; i < remark.Length; i++)
-            {
-                if (char.IsDigit(remark[i]))
-                {
-                    remark = remark.Replace(remark[i], '*');
-                }
-            }
-
-            return remark;
-        }
-
-        
     }
 }
